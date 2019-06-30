61528519dd29e593d4752f0d09c8fae0f3957746
NJ <nalani.jayasundara@gmail.com>
2019-06-30 10:40:14 +0530

    HR1

---
+++ CC1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CC1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] intArray = new int[] { 1, 2, 3, 4};
+          
+            string[] stringArray = new string[] { "nalani","osi", "amma"};
+          
+            PrintArray<Int32>(intArray);
+            PrintArray<String>(stringArray);
+            Console.ReadLine();
+        }
+        static void PrintArray<T>(T[] array)
+        {
+            foreach(var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
